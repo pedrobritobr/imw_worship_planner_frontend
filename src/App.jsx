d0aa6319e6ed1d3ff1e39cf7729a881b40dbd983
pedrorbito <pedroab0119@gmail.com>
2023-10-26 16:00:14 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from './Components/Form';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Cronograma do Culto</h2>
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;

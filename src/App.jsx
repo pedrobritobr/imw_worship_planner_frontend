45f73ff46e65b89b2dc93b2b75f246c1081dc1e2
Pedro Brito <pedroab0119@gmail.com>
2024-10-22 12:32:34 -0300

    corrigido lint

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,7 @@ function App() {
       <div className="main">
         <label htmlFor="worshipTitleInput">
           <h2>Cronograma</h2>
-          <br/>
+          <br />
           <input
             type="text"
             id="worshipTitleInput"

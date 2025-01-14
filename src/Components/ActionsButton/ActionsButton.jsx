99898ffccca8c39ec4e871d6f421d565b1fd9700
Pedro Brito <pedroab0119@gmail.com>
2025-01-13 21:02:16 -0300

    add modal de confirmacao para exclusao

--- src/Components/ActionsButton/ActionsButton.jsx
+++ src/Components/ActionsButton/ActionsButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import trashBtnSvg from '../../assets/trash-bin-2-svgrepo-com.svg';
@@ -8,16 +8,14 @@ import saveImageBtnSvg from '../../assets/image-svgrepo-com.svg';
 
 import './ActionsButton.css';
 
-const handleDeleteLocalStorage = () => {
-  localStorage.removeItem('imwWorshipPlanner');
-  window.location.reload();
-};
+function ActionsButton({ importData, exportData, setShowScreeshotTable }) {
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
-function ActionsButton({
-  importData,
-  exportData,
-  setShowScreeshotTable,
-}) {
+  const handleDeleteLocalStorage = () => {
+    localStorage.removeItem('imwWorshipPlanner');
+    window.location.reload();
+  };
+
   return (
     <div className="actions-button-container">
       <div className="cloud-button">
@@ -32,10 +30,38 @@ function ActionsButton({
         <button type="button" className="download-button" onClick={() => setShowScreeshotTable(true)}>
           <img src={saveImageBtnSvg} alt="Baixar Cronograma" />
         </button>
-        <button type="button" onClick={handleDeleteLocalStorage}>
+        <button type="button" onClick={() => setShowConfirmationModal(true)}>
           <img src={trashBtnSvg} alt="Excluir chave do localStorage" />
         </button>
       </div>
+
+      {showConfirmationModal && (
+        <div className="confirmation-modal">
+          <div className="modal-content">
+            <h3>Tem certeza?</h3>
+            <p>Essa ação excluirá todas as atividades salvas e não poderá ser desfeita.</p>
+            <div className="modal-actions">
+              <button
+                type="button"
+                className="confirm-button"
+                onClick={() => {
+                  handleDeleteLocalStorage();
+                  setShowConfirmationModal(false);
+                }}
+              >
+                Confirmar
+              </button>
+              <button
+                type="button"
+                className="cancel-button"
+                onClick={() => setShowConfirmationModal(false)}
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

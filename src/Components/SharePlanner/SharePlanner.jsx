54941fa382b95b944372cbf8cf9b91e593cc00d9
Pedro Brito <pedroab0119@gmail.com>
2025-06-26 00:29:40 -0300

    converte componente em hook

--- src/Components/SharePlanner/SharePlanner.jsx
+++ src/Components/SharePlanner/SharePlanner.jsx
@@ -1,91 +1,78 @@
-import React, { useState, useContext, useEffect } from 'react';
-
+import { useContext } from 'react';
 import { PlannerContext } from '@/Context/PlannerContext';
 import { UserContext } from '@/Context/UserContext';
 import { uploadPlannerToCloud } from '@/service';
 import { validatePlanner } from '@/helpers';
 import { useDialog } from '@/Context/DialogContext';
 
-function SharePlanner() {
+function useSharePlanner() {
   const { setCurrentPage, pages } = useContext(UserContext);
   const { planner } = useContext(PlannerContext);
   const { showDialog } = useDialog();
-  const [isSharing, setIsSharing] = useState(false);
 
-  useEffect(() => {
-    const sharePlanner = async () => {
-      const { creator, ...plannerWithoutCreator } = planner;
-      const errorMsg = 'Erro ao salvar o cronograma. Tente novamente mais tarde.';
-      try {
-        const isValidPlanner = validatePlanner(plannerWithoutCreator);
+  const sharePlanner = async () => {
+    const { creator, ...plannerWithoutCreator } = planner;
+    const errorMsg = 'Erro ao salvar o cronograma. Tente novamente mais tarde.';
+    try {
+      const isValidPlanner = validatePlanner(plannerWithoutCreator);
 
-        if (isValidPlanner) {
-          showDialog({
-            type: 'alert',
-            title: 'Não é possivel compartilhar o cronograma.',
-            message: `${isValidPlanner}`,
-            autoClose: true,
-            autoCloseTimeout: 30,
-            onCancel: () => setCurrentPage(pages.Home),
-          });
-          return;
-        }
-
-        setIsSharing(true);
-        const response = await uploadPlannerToCloud(plannerWithoutCreator);
-
-        if (!response) {
-          showDialog({
-            type: 'alert',
-            title: 'Atenção',
-            message: errorMsg,
-            autoClose: true,
-            onCancel: () => setCurrentPage(pages.Home),
-          });
-          return;
-        }
-      } catch (error) {
-        console.error('Erro ao compartilhar:', error);
+      if (isValidPlanner) {
         showDialog({
           type: 'alert',
           title: 'Não é possivel compartilhar o cronograma.',
+          message: `${isValidPlanner}`,
+          autoClose: true,
+          autoCloseTimeout: 30,
+          onCancel: () => setCurrentPage(pages.Home),
+        });
+        return;
+      }
+
+      const response = await uploadPlannerToCloud(plannerWithoutCreator);
+
+      if (!response) {
+        showDialog({
+          type: 'alert',
+          title: 'Atenção',
           message: errorMsg,
           autoClose: true,
           onCancel: () => setCurrentPage(pages.Home),
         });
         return;
-      } finally {
-        setIsSharing(false);
       }
-
-      if (navigator.share) {
-        try {
-          await navigator.share({
-            title: 'IMW Cronograma de culto',
-            text: 'Confira o cronograma de culto!',
-            url: `${window.location.href}?shared=true`,
-          });
-        } catch (error) {
-          console.error('Erro ao compartilhar:', error);
-        }
-      } else {
-        showDialog({
-          type: 'alert',
-          title: 'Não é possivel compartilhar o cronograma.',
-          message: 'Compartilhamento não suportado neste dispositivo.',
-          autoClose: true,
-          onCancel: () => setCurrentPage(pages.Home),
+    } catch (error) {
+      console.error('Erro ao compartilhar:', error);
+      showDialog({
+        type: 'alert',
+        title: 'Não é possivel compartilhar o cronograma.',
+        message: errorMsg,
+        autoClose: true,
+        onCancel: () => setCurrentPage(pages.Home),
+      });
+      return;
+    }
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: 'IMW Cronograma de culto',
+          text: 'Confira o cronograma de culto!',
+          url: `${window.location.href}?shared=true`,
         });
+      } catch (error) {
+        console.error('Erro ao compartilhar:', error);
       }
-    };
+    } else {
+      showDialog({
+        type: 'alert',
+        title: 'Não é possivel compartilhar o cronograma.',
+        message: 'Compartilhamento não suportado neste dispositivo.',
+        autoClose: true,
+        onCancel: () => setCurrentPage(pages.Home),
+      });
+    }
+  };
 
-    sharePlanner();
-    setCurrentPage(pages.Home);
-  }, []);
-
-  if (isSharing) return (<span className="loader" />);
-
-  return (<div />);
+  return sharePlanner;
 }
 
-export default SharePlanner;
+export default useSharePlanner;
